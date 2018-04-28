d69c2918511a40be4c2bc79a728c362dd45ac3eb
Michał Budzyński <michal@virtualdesign.pl>
2018-04-28 13:42:00 +0100

    :tiger2: Java part of the plugin

---
+++ RemFixer.java
@@ -0,0 +1,52 @@
+package org.michalbe.cordova.plugin;
+
+import org.apache.cordova.DroidGap;
+import org.apache.cordova.api.CallbackContext;
+import org.apache.cordova.api.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.util.SparseArray;
+import android.webkit.WebSettings;
+import android.webkit.WebSettings.TextSize;
+import android.webkit.WebView;
+
+
+/**
+ * Cordova Plugin for adjusting the font size, between 50% and 200%.
+ * 
+ * Shows up in the JavaScript as window.plugins.fontSizeAdjust
+ * 
+ * @author nolan
+ *
+ */
+@SuppressWarnings("deprecation")
+public class RemFixerPlugin extends CordovaPlugin {
+    
+    @Override
+    public boolean execute() {
+        
+        DroidGap droidGap = (DroidGap)super.cordova.getActivity();
+        
+        WebView webView = (WebView) droidGap.findViewById(100);
+
+        
+        adjustOnUIThread(droidGap, webView);
+        
+        return true;
+    }
+    
+    private void adjustOnUIThread(final Activity activity, final WebView webView) {
+        
+        activity.runOnUiThread(new Runnable() {
+            
+            @Override
+            public void run() {
+                webView.getSettings().setMinimumFontSize(1);
+                webView.getSettings().setMinimumLogicalFontSize(1);
+            }
+        });
+    }
+}
